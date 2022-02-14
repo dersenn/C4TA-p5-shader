2ea6a7af9f0597a08425e6f8a8d2da1ca737e157
dersenn <67318888+dersenn@users.noreply.github.com>
2022-02-14 19:57:10 +0100

    okay. this working now. but possibly not the right approach :-)

--- 03-dimensions-2/shader.frag
+++ 03-dimensions-2/shader.frag
@@ -7,36 +7,33 @@ precision mediump float;
 varying vec2 vTexCoord;
 
 // Per frame info from sketch.js
-
-uniform vec2 u_position;
-uniform vec2 u_dimension;
-
 uniform vec2 u_resolution;
 uniform float u_time;
 uniform vec2 u_mouse;
+
+uniform vec2 u_position;
+uniform vec2 u_dimension;
 
 
 // do shit with the pixel
 void main() {
   // position of the pixel. pixel divided by resolution to get normalized value.
-  vec2 st = (gl_FragCoord.xy / u_resolution.xy) + u_mouse / 4.0;
+  vec2 st = (gl_FragCoord.xy / u_resolution.xy);
 
   // always normalize values coming from p5.
   vec2 pos = u_position.xy / u_resolution.xy;
   // vec2 pos = u_mouse.xy / u_resolution.xy;
   vec2 dim = u_dimension.xy / u_resolution.xy;
+  vec2 mse = u_mouse.xy / u_resolution.xy;
+
+  float r = mse.y;
+  // float g = smoothstep(pos.x, pos.x + mse.x, st.x);
+  float g = smoothstep(0.0, pos.x + mse.x, st.x);
 
 
-  // vec4 rect = vec4(pos.x, pos.y, pos.xy + dim.xy);
-  vec4 rect = vec4(0.2, 0.3, 0.4, 0.5);
-  vec2 hv = step(rect.xy, st) * step(st, rect.zw);
-  float onOff = hv.x * hv.y;
-
-
-
-  // vec3 color = vec3(r, g, 0.0);
+  vec3 color = vec3(r, g, 0.0);
 
   // this can only exist once! it colors the pixel.
-  gl_FragColor = mix(vec4(0,0,0,0), vec4(1,0,0,0), onOff);
+  gl_FragColor = vec4(color, 1.0);
 
 }
--- 03-dimensions-2/sketch.js
+++ 03-dimensions-2/sketch.js
@@ -7,6 +7,13 @@ let canMin = Math.min(canW, canH) //shorter canvas side
 
 // Shader Vars
 let theShader
+let theTexture
+
+let zero
+
+let shapes = []
+let nCols = 5
+let nRows = 1
 
 function preload() {
   theShader = loadShader('shader.vert', 'shader.frag')
@@ -19,42 +26,62 @@ function setup() {
   let canvas = createCanvas(canW, canH, WEBGL)
   canvas.parent(container)
 
+  zero = {
+    x: -width/2.0,
+    y: -height/2.0,
+    z: -width/2.0
+  }
+
+  for (let x = 0; x < nCols; x++) {
+    for (let y = 0; y < nRows; y++) {
+      shapes.push({
+        x: zero.x + x*(width/nCols),
+        y: zero.y + y*(height/nRows),
+        w: width/nCols,
+        h: height/nRows
+      })
+    }
+  }
+  console.log(shapes)
+
   // disable scaling for retina etc.
   pixelDensity(1)
+
+  theTexture = createGraphics(width, height, WEBGL)
+  theTexture.noStroke()
 
   noStroke()
 }
 
 
-let shape = {
-  x: 100.0,
-  y: 100.0,
-  w: 100.0,
-  h: 100.0
-}
 
 // p5 Draw
 function draw() {
+  background(255)
+
+  theTexture.shader(theShader)
 
   // send per frame info of sketch to shader.frag
-  theShader.setUniform("u_position", [shape.x, shape.y]);
-  theShader.setUniform("u_dimension", [shape.w, shape.h]);
-  
   theShader.setUniform("u_resolution", [width, height]);
-  theShader.setUniform("u_time", millis() / 1000.0); // we divide millis by 1000 to convert it to seconds
+  theShader.setUniform("u_time", millis() / 1000.0);
   theShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]); // we flip Y so it's oriented properly in our shader
 
+  theTexture.rect(0, 0, width, height);
 
   // this be like fill
-  shader(theShader)
+  texture(theTexture)
 
-  // the shape to draw onto.
-  // but it doesn't really affect the shader.
-  // guess i must define this otherwise...
+  // push()
+  for (let i = 0; i < shapes.length; i++) {
+    let s = shapes[i]
 
-  rect(shape.x, shape.y, shape.w, shape.h)
+    theShader.setUniform("u_position", [s.x, s.y]);
+    theShader.setUniform("u_dimension", [s.w, s.h]);
 
-  // rect(0, 0, width, height)
+    rect(s.x, s.y, s.w, s.h)
+  }
+  // pop()
+
 }
 
 
